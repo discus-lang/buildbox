4dee7d97f4d48c8f336aabdbf82f304f4ba5066b
benl <benl@ouroborus.net>
2010-11-08 21:20:58 +1100

    Add missing file
    Ignore-this: 3f37a7b4d1b4efd88d5ad25f03c7f62f
    
    darcs-hash:20101108102058-bf82d-856d1368a6dd1df661b4e22064bcd35e3e6d2e36

---
+++ BuildBox/BuildBox/Aspect/Stats.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE GADTs #-}
+
+-- | Extracting statistics from many-valued data.
+module BuildBox.Aspect.Stats
+	( Stats	(..)
+	, makeStats)
+where
+import BuildBox.Pretty
+import BuildBox.Data.Dividable
+
+-- | Minimum, average and maximum values.
+data Stats a	
+	= Stats
+	{ statsMin	:: a
+	, statsAvg	:: a
+	, statsMax	:: a }
+	deriving (Read, Show)
+
+instance Pretty a => Pretty (Stats a) where
+	ppr (Stats mi av mx)
+		=  ppr mi <> text "/" 
+		<> ppr av <> text "/"
+		<> ppr mx
+
+
+-- | Make stats from a list of values.
+makeStats :: (Num a, Dividable a, Ord a) => [a] -> Stats a
+makeStats xs
+	= Stats (minimum xs)
+		(sum xs `divide` (fromIntegral $ length xs)) 
+		(maximum xs)
+
+
+	
